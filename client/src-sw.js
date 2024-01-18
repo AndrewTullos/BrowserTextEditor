3f0d55923caa7ce8506144cd78cd897957d3c457
Drew Tullos <tullos.andrew@gmail.com>
2024-01-17 20:57:25 -0600

    Updated commit

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,30 +1,43 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Cache strategy for pages
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
+	cacheName: "page-cache",
+	plugins: [
+		new CacheableResponsePlugin({
+			statuses: [0, 200],
+		}),
+		new ExpirationPlugin({
+			maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+		}),
+	],
 });
 
+// Pre-caching pages
 warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
+	urls: ["/index.html", "/"],
+	strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+// Caching for navigational requests
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Asset Caching: Caching for styles, scripts, and workers
+registerRoute(
+	({ request }) => ["style", "script", "worker"].includes(request.destination),
+	new StaleWhileRevalidate({
+		cacheName: "asset-cache",
+		plugins: [
+			new CacheableResponsePlugin({
+				statuses: [0, 200],
+			}),
+		],
+	})
+);
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,5 +1,7 @@
 const butInstall = document.getElementById("buttonInstall");
 
+let deferredPrompt;
+
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
@@ -9,7 +11,17 @@ window.addEventListener("beforeinstallprompt", (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener("click", async () => {});
+butInstall.addEventListener("click", async () => {
+	if (deferredPrompt) {
+		deferredPrompt.prompt();
+		const { promptEvent } = await deferredPrompt.userChoice;
+		console.log(`User response to the install prompt: ${promptEvent}`);
+		deferredPrompt = null;
+	}
+	butInstall.style.display = "none";
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener("appinstalled", (event) => {});
+window.addEventListener("appinstalled", (event) => {
+	console.log("PWA installed");
+});
